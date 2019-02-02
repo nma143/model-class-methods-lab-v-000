0b83e2772c51926816bf4901eb199094aa62d5d5
Nikki A <nikki.apostolakis@gmail.com>
2019-02-02 19:44:20 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -27,4 +27,9 @@ class Boat < ActiveRecord::Base
    self.includes(:classifications).where(classifications: { name: 'Sailboat' })
  end
 
+ def self.with_three_classifications
+  #  joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+  binding.pry
+ end
+
 end
